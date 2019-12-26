80f50f2e565ba9035f7c6bfbaef2776f8fd6605a
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-26 20:06:05 +0300

    afdasfd

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 document.getElementById('proverca').onclick = function () {
     var x = document.getElementById('years').value;
     x = parseInt(x);
+
+    if (x >= 16) {
+        alert('добро пожаловать!');
+    } else {
+        alert('Вход воспрещён');
+    };
 };
